97398882f13c3fede882f9a937044a8f3d7d605d
LeeYounHyoung <rkwkskeofh9@naver.com>
2025-02-08 21:51:52 +0900

    Initial Commit

---
+++ LetterApp/LetterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetterAppApp.swift
+//  LetterApp
+//
+//  Created by 이윤형 on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LetterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
